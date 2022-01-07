462e5f29b4a51e5f73a90f2fb5514c254ce770d7
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-07 18:53:50 +0530

    Time: 12 ms (90.81%), Space: 26.3 MB (46.58%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_map<int, int> m;
+    
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int start, int end, int& index) {
+        
+        if(start > end) return NULL;
+        
+        TreeNode* root = new TreeNode(preorder[index++]);
+        
+        int rootInd = m[root->val];
+        
+        root->left = build(preorder, inorder, start, rootInd-1, index);
+        root->right = build(preorder, inorder, rootInd+1, end, index);
+        
+        return root;
+        
+        
+//         int leftMid = index / 2;
+//         int rightMid = (index + 1 + n) / 2;
+        
+//         if(m[inorder[leftMid]] == false) {
+//             root->left = new TreeNode(inorder[leftMid]);
+//             m[inorder[leftMid]] = true;
+//             build(root->left, preorder, inorder, 0, leftMid);
+//         }
+//         if(m[inorder[rightMid]] == false) {
+//             root->left = new TreeNode(inorder[rightMid]);
+//             m[inorder[rightMid]] = true;
+//             build(root->left, preorder, inorder, index, n);
+//         }
+        
+        
+    }
+    
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+        int n = preorder.size();
+        TreeNode* result = new TreeNode(preorder[0]);
+        
+        if(n == 1) return result;
+        
+        for(int i=0 ; i<n ; i++) {
+            m[inorder[i]] = i;
+        }
+        
+        int index = 0;
+        return build(preorder, inorder, 0, n-1, index);
+
+    }
+};
