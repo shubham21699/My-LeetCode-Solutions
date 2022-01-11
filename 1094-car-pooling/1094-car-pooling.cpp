ec4a98a27ea8fe3171e011adf32b1b420b9e2419
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-11 14:06:44 +0530

    Time: 19 ms (53.45%), Space: 9.8 MB (93.99%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -8,20 +8,20 @@ public:
             return a[1] < b[1];
         });
         
-        int current = 0;
+        int current_passengers = 0;
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
         
         for(int i=0 ; i<n ; i++) {
             
             while(!pq.empty() && pq.top().first <= trips[i][1]) {
-                current -= pq.top().second;
+                current_passengers -= pq.top().second;
                 pq.pop();
             }
             
             pq.push({ trips[i][2], trips[i][0] });
-            current += trips[i][0];
+            current_passengers += trips[i][0];
             
-            if(current > capacity) return false;
+            if(current_passengers > capacity) return false;
             
         }
         
