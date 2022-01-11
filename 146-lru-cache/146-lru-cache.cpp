09c04756b01e49d8c7d17251d3eb5e623e595c36
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-11 23:01:54 +0530

    Time: 424 ms (62.58%), Space: 174.2 MB (33.85%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,111 +1,58 @@
 class LRUCache {
 public:
-    // unordered_map<int, pair<list<int>::iterator, int>> m;
-    // list<int> l;
-    // int max_capacity = 0;
-    list<pair<int, int>> lst;
-    // Pair of key, value
-    
-    // Hashtable
-    vector<list<pair<int, int>>::iterator> hshtable;
-    int mxLimit;
+    unordered_map<int, pair<list<int>::iterator, int>> m;
+    list<int> l;
+    int max_capacity = 0;
     
     LRUCache(int capacity) {
-        // m.clear();
-        // l.clear();
-        // max_capacity = capacity;
-        mxLimit = capacity;
-        lst.clear();
-        hshtable.clear();
-        hshtable.resize(10001);
-        fill(hshtable.begin(), hshtable.end(), lst.end());
+        m.clear();
+        l.clear();
+        max_capacity = capacity;
     }
     
     int get(int key) {
         
-        if(hshtable[key] != lst.end()) {
-            // The value already exists
-            // Move it to the back
-            auto it = hshtable[key];
-            int value = it->second;
-            lst.erase(it);
+        if(m.find(key) != m.end()) {
             
-            // This value does not exist in the list
-            // Add it
-            lst.push_back({key, value});
-            it = --lst.end();
-            hshtable[key] = it;
+            l.erase(m[key].first);
+            l.push_front(key);
+            m[key].first = l.begin();
+            
+            return m[key].second;
         }
         
-        
-        if (hshtable[key] == lst.end()) return -1;
-        else {
-            return hshtable[key]->second;
-        }
-        
-//         if(m.find(key) != m.end()) {
-            
-//             l.erase(m[key].first);
-//             l.push_front(key);
-//             m[key].first = l.begin();
-            
-//             return m[key].second;
-//         }
-        
-//         return -1; 
+        return -1; 
     }
     
     void put(int key, int value) {
         
-        if(hshtable[key] != lst.end()) {
-            // The value already exists
-            // Move it to the back
-            auto it = hshtable[key];
-            lst.erase(it);
+        // If already present, then update it
+        if(m.find(key) != m.end()) {
+            
+            m[key].second = value;
+            
+            l.erase(m[key].first);
+            l.push_front(key);
+            m[key].first = l.begin();
+            
         }
         
-        // This value does not exist in the list
-        // Add it
-        lst.push_back({key, value});
-        auto it = --lst.end();
-        hshtable[key] = it;
-        
-        if (lst.size() > mxLimit) {
-            // We need to pop the value from the front
-            auto it = lst.begin();
-            hshtable[it->first] = lst.end();
-
-            lst.pop_front();
+        else {
+            
+            l.push_front(key);
+            m[key].first = l.begin();
+            m[key].second = value;
+            max_capacity--;
+            
         }
-
         
-//         // If already present, then update it
-//         if(m.find(key) != m.end()) {
+        if(max_capacity < 0) {
             
-//             m[key].second = value;
+            m.erase(l.back());
+            l.pop_back();
+            max_capacity++;
             
-//             l.erase(m[key].first);
-//             l.push_front(key);
-//             m[key].first = l.begin();
-            
-//         }
-        
-//         else {
-            
-//             l.push_front(key);
-//             m[key].first = l.begin();
-//             m[key].second = value;
-//             max_capacity--;
-            
-//         }
-        
-//         if(max_capacity < 0) {
-            
-//             m.erase(l.back());
-//             l.pop_back();
-//             max_capacity++;
-            
-//         }
+        }
         
     }
 };
