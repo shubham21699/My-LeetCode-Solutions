80242ddf2fa144be44196981ae10a8f0662b1a69
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-13 18:52:47 +0530

    Time: 202 ms (14.32%), Space: 31 MB (9.77%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
@@ -6,48 +6,63 @@ class Solution {
         
         if(x >=0 && x < n && y >=0 && y < n) return true;
         return false;
-        
     }
+    
+    unordered_map<int, unordered_map<int, unordered_map<int, double> > > dp;
     
 public:
     double knightProbability(int n, int k, int row, int column) {
         
+        if(!isSafe(row, column, n)) return 0;
+        if(dp.count(row) && dp[row].count(column) && dp[row][column].count(k)) return dp[row][column][k];
         if(k == 0) return 1;
         
-        vector<vector<vector<double>> > dp(n, vector<vector<double> >(n, vector<double>(k+1, 0)));
+        double total_possible = knightProbability(n, k-1, row-2, column+1) 
+                                + knightProbability(n, k-1, row-1, column+2) 
+                                + knightProbability(n, k-1, row+1, column+2) 
+                                + knightProbability(n, k-1, row+2, column+1) 
+                                + knightProbability(n, k-1, row+2, column-1) 
+                                + knightProbability(n, k-1, row+1, column-2) 
+                                + knightProbability(n, k-1, row-1, column-2) 
+                                + knightProbability(n, k-1, row-2, column-1);
         
-        for(int i=0 ; i<n ; i++) {
-            for(int j=0 ; j<n ; j++) {
-                dp[i][j][0] = 1;
-            }
-        }
-        
-        for(int steps=1 ; steps<=k ; steps++) {
-            
-            for(int i=0 ; i<n ; i++) {
-                for(int j=0 ; j<n ; j++) {
-                    
-                    double prob = 0.0;
-
-                    for(int dir=0 ; dir<8 ; dir++) {
-
-                        int newRow = i + nx[dir];
-                        int newCol = j + ny[dir];
-                        
-                        if(isSafe(newRow, newCol, n)) {
-                            
-                            prob += (dp[newRow][newCol][steps-1] / 8.0);
-                            
-                        }
-
-                    }
-
-                    dp[i][j][steps] = prob;
-                }
-            }
-        }
-        
+        dp[row][column][k] = total_possible / 8.0;
         return dp[row][column][k];
         
+        
+        
+////////////////// OR ////////////////////////////
+//         vector<vector<vector<double>> > dp(n, vector<vector<double> >(n, vector<double>(k+1, 0)));
+        
+//         for(int i=0 ; i<n ; i++) {
+//             for(int j=0 ; j<n ; j++) {
+//                 dp[i][j][0] = 1;
+//             }
+//         }
+        
+//         for(int steps=1 ; steps<=k ; steps++) {
+            
+//             for(int i=0 ; i<n ; i++) {
+//                 for(int j=0 ; j<n ; j++) {
+                    
+//                     double prob = 0.0;
+
+//                     for(int dir=0 ; dir<8 ; dir++) {
+
+//                         int newRow = i + nx[dir];
+//                         int newCol = j + ny[dir];
+                        
+//                         if(isSafe(newRow, newCol, n)) {
+//                             prob += (dp[newRow][newCol][steps-1] / 8.0);                     
+//                         }
+
+//                     }
+
+//                     dp[i][j][steps] = prob;
+//                 }
+//             }
+//         }
+        
+//         return dp[row][column][k];
     }
 };
