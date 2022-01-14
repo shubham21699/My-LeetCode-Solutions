54fcde9cd532554e0afbea3b2200992582e4acb3
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-14 23:11:17 +0530

    Time: 10 ms (53.94%), Space: 10.8 MB (73.45%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    void find(vector<int>& candidates, int target, vector<vector<int> >& result, vector<int>& v, int index, int n, int sum) {
+        
+        if(sum == target) {
+            result.push_back(v);
+            return;
+        }
+        if(sum > target || index >= n) {
+            return;
+        }
+        
+        
+        for(int i=index ; i<n ; i++) {
+            v.push_back(candidates[i]);
+
+            find(candidates, target, result, v, i, n, sum+candidates[i]);
+
+            v.pop_back();
+            
+        }
+        
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        int n = candidates.size();
+        vector<vector<int> > result;
+        
+        if(n == 1) {
+            if(target < candidates[0]) {
+                return result;
+            }
+        }
+        
+        vector<int> v;
+        int sum = 0;
+        find(candidates, target, result, v, 0, n, sum);
+        return result;
+    }
+};
