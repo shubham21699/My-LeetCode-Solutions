2a39fc5933a4fe034b7a55f69818db68df3d2e60
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-14 18:42:10 +0530

    Time: 63 ms (79.25%), Space: 141 MB (6.01%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool find(vector<int>& nums, int sum, int req_sum, int index, int n, vector<vector<int> >& dp) {
+        
+        if(sum == req_sum) return true;
+        if(index >= n || sum > req_sum) return false;
+        if(dp[index][sum] != -1) return dp[index][sum];
+        
+        return dp[index][sum] = find(nums, sum+nums[index], req_sum, index+1, n, dp) || find(nums, sum, req_sum, index+1, n, dp);
+        // return dp[index][sum];
+    }
+    
+public:
+    bool canPartition(vector<int>& nums) {
+        
+        int n = nums.size();
+        int sum = 0;
+        
+        for(int i=0 ; i<n ; i++) sum += nums[i];
+        
+        if(sum % 2 != 0) return false;
+        
+//         int bitsum = nums[0];
+//         for(int i=1 ; i<n ; i++) {
+//             bitsum |= nums[i];
+//         }
+        
+//         if(bitsum) return true;
+//         return false;
+        
+        int req_sum = sum/2;
+        
+        vector<vector<int> > dp(n+1, vector<int>(sum + 1, -1));
+        sum = 0;
+        
+        return find(nums, sum, req_sum, 0, n, dp);
+        
+    }
+};
