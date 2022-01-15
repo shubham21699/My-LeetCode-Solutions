f93bc655bde39d969982171679506ad89d84a9ab
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-15 23:08:13 +0530

    Time: 893 ms (8.87%), Space: 90.5 MB (18.88%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -7,8 +7,7 @@ public:
         sort(intervals.begin(), intervals.end());
         intervals.erase(unique(intervals.begin(), intervals.end()), intervals.end());
         
-        int diff = n - intervals.size();
-        int count = diff;
+        int count = n - intervals.size();
         n = intervals.size();
         
         if(n == 1) return count;
@@ -17,13 +16,12 @@ public:
         s.push(intervals[0]);
         
         for(int i=1 ; i<n ; i++) {
-            vector<int> r = s.top();
-            if(intervals[i][0] >= r[1]) {
+            if(intervals[i][0] >= s.top()[1]) {
                 s.push(intervals[i]);
             }
             else {
                 count++;
-                if(intervals[i][1] < r[1]) {
+                if(intervals[i][1] < s.top()[1]) {
                     s.pop();
                     s.push(intervals[i]);
                 }
