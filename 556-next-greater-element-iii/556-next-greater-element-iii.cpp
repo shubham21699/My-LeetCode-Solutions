77f04ed67c7484e23d609e3fd8779b13baf3d99b
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-15 12:50:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (68.03%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -15,7 +15,26 @@ public:
             else return -1;
         }
         
-        next_permutation(s.begin(), s.end());
+        // One method to get next permutation
+        int i = len-2;
+        while(i>=0 && s[i] >= s[i+1]) {
+            i--;
+        }
+        
+        if(i == -1) return -1;
+        
+        reverse(s.begin()+i+1, s.end());
+        
+        for(int j=i+1 ; j<len ; j++) {
+            if(s[j] > s[i]) {
+                swap(s[j], s[i]);
+                break;
+            }
+        }
+        
+        // OR : Another method to get next permutation is to use built-in function
+        // next_permutation(s.begin(), s.end());
+        
         int result = stoll(s);
         
         return (result <= n) ? -1 : result;
