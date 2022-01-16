08ec757c2d8e73470b7db2c3f060aaf30dab08b7
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-16 12:54:41 +0530

    Time: 94 ms (23.22%), Space: 49.8 MB (45.33%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -4,7 +4,7 @@ public:
         
         int n = gas.size();
         int capacity = 0;
-        int result = 0;
+        int start_point = 0;
         int deficit_amount = 0;
         
         for(int i=0 ; i<n ; i++) {
@@ -14,13 +14,13 @@ public:
             if(capacity < 0) {
                 deficit_amount += capacity;
                 capacity = 0;
-                result = i+1;
+                start_point = i+1;
             }
             
         }
         
         deficit_amount += capacity;
         
-        return deficit_amount<0 ? -1 : result;
+        return deficit_amount<0 ? -1 : start_point;
     }
 };
