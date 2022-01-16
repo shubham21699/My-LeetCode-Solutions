4314d341084731df3d8fb3415ba2fcc83e074f84
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-16 14:34:41 +0530

    Time: 64 ms (66.28%), Space: 30.4 MB (32.03%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -5,7 +5,9 @@ public:
         int n = nums.size();
         priority_queue<int, vector<int>, greater<int> > pq;
         
-        for(int i=0 ; i<n ; i++) pq.push(nums[i]);
+        for(int i=0 ; i<n ; i++) {
+            pq.push(nums[i]);
+        }
         
         for(int i=0 ; i<n ; i++) {
             nums[i] = pq.top();
