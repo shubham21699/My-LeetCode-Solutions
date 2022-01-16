ca46af9bbe4607e1f0804b363a8ab453d2ba8fc6
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-16 19:20:55 +0530

    Time: 48 ms (35.13%), Space: 13.1 MB (74.21%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -6,20 +6,46 @@ public:
         if(k == 1) return matrix[0][0];
         if(k == n*n) return matrix[n-1][n-1];
         
-        priority_queue<int, vector<int>, greater<int> > pq;
+        auto cmp = [&](pair<int,int> a,pair<int,int> b){
+            return matrix[a.first][a.second] > matrix[b.first][b.second];
+        };
+    
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp) > pq(cmp);
         
         for(int i=0 ; i<n ; i++) {
-            for(int j=0 ; j<n ; j++) {
-                pq.push(matrix[i][j]);
-            }
+            pq.push({ i, 0 });
         }
         
         while(k > 1) {
+            
+            pair<int, int> p = pq.top();
             pq.pop();
+            
+            if(p.second+1 < n) {
+                pq.push({ p.first, p.second+1 });
+            }
+            
             k--;
         }
         
-        return pq.top();
+        return matrix[pq.top().first] [pq.top().second];
+        
+        
+//         OR:
+//         priority_queue<int, vector<int>, greater<int> > pq;
+        
+//         for(int i=0 ; i<n ; i++) {
+//             for(int j=0 ; j<n ; j++) {
+//                 pq.push(matrix[i][j]);
+//             }
+//         }
+        
+//         while(k > 1) {
+//             pq.pop();
+//             k--;
+//         }
+        
+//         return pq.top();
         
     }
 };
