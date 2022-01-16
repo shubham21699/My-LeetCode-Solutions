695ec3e73a230be9227ac44468a892ddf4b998aa
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-16 21:09:58 +0530

    Time: 28 ms (84.14%), Space: 13.2 MB (74.21%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,4 +1,37 @@
 class Solution {
+    int getEleGreaterThanOrEqualToMid(vector<vector<int> >& matrix, int n, int num) {
+        
+        int result = 0;
+        
+        for(int i=0 ; i<n ; i++) {
+            
+            // If num is less than first element, then no more element are 
+            // futher less than or equal or equal to num
+            if(num < matrix[i][0]) return result;
+            
+            // If num is greater than last element, then it is greater than 
+            // all the elements in that row
+            if(num >= matrix[i][n-1]) {
+                result += n;
+                continue;
+            }
+            
+            // This contain the column index of last element in matrix less than of equal to num
+            int greaterThan = 0;
+            for(int jump=n/2 ; jump>=1 ; jump/=2) {
+                
+                while(greaterThan+jump < n && matrix[i][greaterThan+jump] <= num) {
+                    greaterThan += jump;
+                }
+                
+            }
+            
+            result += greaterThan+1;
+        }
+        
+        return result;
+    }
+    
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         
@@ -6,33 +39,27 @@ public:
         if(k == 1) return matrix[0][0];
         if(k == n*n) return matrix[n-1][n-1];
         
-        auto cmp = [&](pair<int,int> a,pair<int,int> b){
-            return matrix[a.first][a.second] > matrix[b.first][b.second];
-        };
+        
+        // Using Binary Search:
+        int left = matrix[0][0];
+        int right = matrix[n-1][n-1];
+        
+        while(left <= right) {
+            
+            int mid = left + (right - left)/2;
+            
+            int greaterThanOrEqualToMid = getEleGreaterThanOrEqualToMid(matrix, n, mid);
+            
+            if(greaterThanOrEqualToMid >= k) right = mid-1;
+            else left = mid+1;
+            
+        }
+        
+        return left;
+
     
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp) > pq(cmp);
         
-        for(int i=0 ; i<n ; i++) {
-            pq.push({ i, 0 });
-        }
-        
-        while(k > 1) {
-            
-            pair<int, int> p = pq.top();
-            pq.pop();
-            
-            if(p.second+1 < n) {
-                pq.push({ p.first, p.second+1 });
-            }
-            
-            k--;
-            
-        }
-        
-        return matrix[pq.top().first] [pq.top().second];
-        
-        
-//         OR:
+//         OR: Using Heap Sort
 //         priority_queue<int, vector<int>, greater<int> > pq;
         
 //         for(int i=0 ; i<n ; i++) {
