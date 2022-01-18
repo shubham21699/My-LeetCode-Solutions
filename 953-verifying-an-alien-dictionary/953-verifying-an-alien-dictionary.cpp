272c0ae90bc1d2b8e9ce45f12c2cf43dea19e434
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-18 17:12:22 +0530

    Time: 16 ms (6.22%), Space: 9.7 MB (41.37%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -15,11 +15,13 @@ public:
             int n = min(words[i].length(), words[i+1].length());
             
             for(int j=0 ; j<n ; j++) {
+                
                 if(m[words[i][j]] < m[words[i+1][j]]) {
                     flag = true;
                     break;
                 }
                 else if(m[words[i][j]] > m[words[i+1][j]]) return false;
+                
             }
             
             if(flag == false && words[i].length() > words[i+1].length()) return false;
