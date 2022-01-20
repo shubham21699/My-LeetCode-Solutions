cb4f9543323e4ef4de48ca8fec2d762f43155ad9
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-20 17:44:28 +0530

    Time: 3 ms (25.96%), Space: 6.7 MB (36.52%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    unordered_map<char, string> m = {
+        { '2', "abc" },
+        { '3', "def" },
+        { '4', "ghi" }, 
+        { '5', "jkl" },
+        { '6', "mno" },
+        { '7', "pqrs" },
+        { '8', "tuv" },
+        { '9', "wxyz" }
+    };
+    
+    void find(string digits, string s, vector<string>& result, int index, int n) {
+                
+        if(s.length() == n) {
+            result.push_back(s);
+            return;
+        }
+        
+        if(index >= n) return;
+        
+        string x = m[digits[index]];
+        
+        for(int i=0 ; i<x.length() ; i++) {
+            s += x[i];
+            find(digits, s, result, index+1, n);
+            s.pop_back();
+        }
+        
+    }
+    
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        int n = digits.length();
+        vector<string> result;
+        
+        if(n == 0) return result;
+        if(n == 1) {
+            string s = m[digits[0]];
+            for(int i=0 ; i<s.length() ; i++) {
+                string x = "";
+                result.push_back(x + s[i]);
+            }
+            return result;
+        }
+        
+        string s = "";
+        find(digits, s, result, 0, n);
+        return result;
+    }
+};
