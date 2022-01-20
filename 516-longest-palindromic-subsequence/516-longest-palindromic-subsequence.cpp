4c89533cb4db6c23fad9639101b6c00c87727bf4
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-20 15:53:02 +0530

    Time: 88 ms (65.54%), Space: 72.9 MB (51.12%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -26,11 +26,14 @@ public:
         vector<vector<int> > dp(n, vector<int>(n, 0));
         
         for(int i=n-1 ; i>=0 ; i--) {
+            
             dp[i][i] = 1;
             
             for(int j=i+1 ; j<n ; j++) {
+                
                 if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2;
                 else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                
             }
             
         }
