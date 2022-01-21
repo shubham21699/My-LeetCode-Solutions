b9aab9a2acbc760069d3b4b7e45e99de135978a4
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-22 00:09:39 +0530

    Time: 42 ms (26.95%), Space: 19.4 MB (13.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -7,6 +7,7 @@ public:
         
         int i = -1;
         
+        // Store the index of 1st occurrence of 0 in array
         for(int j=0 ; j<n ; j++) {
             if(nums[j] == 0) {
                 i = j;
@@ -14,8 +15,10 @@ public:
             }
         }
         
+        // If 0 doesn't exist, then just return
         if(i == -1) return;
         
+        // Else make changes
         for(int j=0 ; j<n ; j++) {
             
             if(nums[j] != 0 && i<j) {
