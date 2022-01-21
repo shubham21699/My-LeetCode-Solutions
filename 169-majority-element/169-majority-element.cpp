c1dbebb62f30730fb7d203ce45b63c39dc9ee50f
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-21 23:18:19 +0530

    Time: 18 ms (68.46%), Space: 19.7 MB (50.43%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -6,11 +6,9 @@ public:
         unordered_map<int, int> m;
         
         for(int i=0 ; i<n ; i++) {
-            
             m[nums[i]]++;
             
             if(m[nums[i]] > floor(n/2)) return nums[i];
-            
         }
         
         return 0;
