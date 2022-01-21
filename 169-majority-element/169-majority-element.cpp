989a88aa421ee96a109a801c10d108411d915e2b
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-21 23:18:01 +0530

    Time: 63 ms (5.54%), Space: 19.7 MB (8.70%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -4,16 +4,15 @@ public:
         
         int n = nums.size();
         unordered_map<int, int> m;
-        int i = 0;
         
-        while(i < n) {
+        for(int i=0 ; i<n ; i++) {
             
             m[nums[i]]++;
-            if(m[nums[i]] > floor(n/2)) break;
             
-            i++;
+            if(m[nums[i]] > floor(n/2)) return nums[i];
+            
         }
         
-        return nums[i];
+        return 0;
     }
 };
