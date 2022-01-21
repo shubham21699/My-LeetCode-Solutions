6185cccf7a37c53468445a9dce65e5655ce5aeb1
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-21 22:53:43 +0530

    Time: 8 ms (96.40%), Space: 8 MB (57.54%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -17,10 +17,12 @@ public:
         ListNode* slow = head;
         
         while(fast->next && fast->next->next) {
+            
             fast = fast->next->next;
             slow = slow->next;
             
             if(fast == slow) return true;
+            
         }
         
         return false;
