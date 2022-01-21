06e011fe16ef8f5274efce17b74ef80df70396c5
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-21 22:45:45 +0530

    Time: 12 ms (97.88%), Space: 17 MB (49.79%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -5,12 +5,10 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
         
-        int ans = 0;
-        
-        for(int i=0 ; i<n ; i++) {
-            ans ^= nums[i];
+        for(int i=1 ; i<n ; i++) {
+            nums[i] ^= nums[i-1];
         }
         
-        return ans;
+        return nums[n-1];
     }
 };
