a6713004ab8111fa6daaca517c4c64b7976410c7
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-23 00:44:37 +0530

    Time: 16 ms (5.88%), Space: 12.1 MB (35.50%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -19,9 +19,7 @@ public:
         
         reverse(nums.begin()+i+1, nums.end());
                 
-        if(i == -1) {
-            return;
-        }
+        if(i == -1) return;
         
         for(int j=i+1 ; j<n ; j++) {
             if(nums[i] < nums[j]) {
