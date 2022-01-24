67f2404654ffe17dafaa8f6a7c594b424fe0a939
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-24 22:47:32 +0530

    Time: 16 ms (75.75%), Space: 16.5 MB (45.36%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n == 1) return 0;        
+        if(n == 2) return 1;
+        
+///////////////  Greedy Approach  ////////////////
+        int steps = 0;
+        int start = 0;
+        int end =0 ;
+        
+        while(end < n-1) {
+            
+            steps++;
+            int maxEnd = end+1;
+            
+            for(int i=start ; i<=end ; i++) {
+                if(i+nums[i] >= n-1) return steps;
+                maxEnd = max(maxEnd, i+nums[i]);
+            }
+            
+            start = end+1;
+            end = maxEnd;
+        }
+        
+        return steps;
+        
+///////////////  OR: DP Approach  ////////////////////
+//         vector<int> jumps(n, INT_MAX);
+//         jumps[0] = 0;
+        
+//         // Find minimum number of jumps to reach nums[i] from nums[0],
+//         // and assign this value to jumps[i]
+//         for(int i=1 ; i<n ; i++) {
+//             for(int j=0 ; j<i ; j++) {
+//                 if(j+nums[j] >= i && jumps[j] != INT_MAX) {
+//                     jumps[i] = min(jumps[i], jumps[j]+1);
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return jumps[n-1];
+        
+    }
+};
