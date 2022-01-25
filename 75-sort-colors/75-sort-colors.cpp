45c580a07e65cd2d45416b43c9978e602f95aeae
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-25 22:21:14 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (68.80%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -13,10 +13,12 @@ public:
         int j = n-1;
         
         for(int k=0 ; k<=j ; k++) {
+            
             while(nums[k] == 2 && k<j) {
                 swap(nums[k], nums[j]);
                 j--;
             }
+            
             while(nums[k] == 0 && k>i) {
                 swap(nums[k], nums[i]);
                 i++;
