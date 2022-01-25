d66655e19b2a0b7d77462c4ecf9851dd75970ad6
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-25 13:37:52 +0530

    Time: 140 ms (24.65%), Space: 48.3 MB (49.86%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -7,7 +7,6 @@ public:
         if(nums[0] == 0) return false;
         
         int distance = 0;
-        
         for(int i=0 ; i<=distance ; i++) {
             distance = max(distance, i+nums[i]);
             if(distance >= n-1) return true;
