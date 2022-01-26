eaa2b60a6bb10d64f58f00920341a555840ffc06
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-26 12:26:35 +0530

    Time: 140 ms (67.65%), Space: 84.3 MB (91.67%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,13 +11,11 @@
  */
 class Solution {
     void Inorder(TreeNode* root, vector<int>& result) {
-        
         if(!root) return;
         
         Inorder(root->left, result);
         result.push_back(root->val);
         Inorder(root->right, result);
-        
     }
     
 public:
