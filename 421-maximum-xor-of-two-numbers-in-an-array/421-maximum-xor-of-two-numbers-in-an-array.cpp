a1d5b6b8cdb9c8316f04d6e6f48e8e4fcee9181e
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-27 13:22:29 +0530

    Time: 1015 ms (11.87%), Space: 92.4 MB (33.67%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int ans = 0, mask = 0;
+        for(int i=31 ; i>=0 ; i--) {
+            mask = mask | (1 << i);
+            int greedyAns = (ans | (1 << i));
+            
+            // Check if possible
+            unordered_set<int> s;
+            for(auto num : nums) {
+                // We basically insert with first i bits conserved and the rest of the bits are 0 after 1
+                s.insert(num & mask);
+            }
+            
+            for(auto prefix : s) {
+                if(s.find(greedyAns xor prefix) != s.end()) {
+                    ans = greedyAns;
+                    break;
+                }
+            }
+            
+        }
+        
+        return ans;
+        
+//         OR: Will give TLE
+//         priority_queue<int> pq;
+        
+//         for(int i=0 ; i<n ; i++) {
+//             for(int j=i ; j<n ; j++) {
+//                 int x = nums[i] xor nums[j];
+//                 pq.push(x);
+//             }
+//         }
+        
+//         return pq.top();
+    }
+};
