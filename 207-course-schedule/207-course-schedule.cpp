e006c615013ab074405aadba06f6ee1167c8a65b
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-28 00:25:17 +0530

    Time: 39 ms (27.37%), Space: 13.1 MB (96.86%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        int n = prerequisites.size();
+        if(n <= 1) return true;
+        
+        vector<int> graph[numCourses];
+        vector<int> indegree(numCourses);
+        
+        for(int i=0 ; i<n ; i++) {
+            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+        
+        queue<int> q;
+        for(int i=0 ; i<numCourses ; i++) {
+            if(indegree[i] == 0) q.push(i);
+        }
+        
+        int count = 0;
+    
+        while(!q.empty()) {
+            
+            int current = q.front();
+            q.pop();
+            count++;
+            
+            for(auto nextCourse : graph[current]) {
+                if(--indegree[nextCourse] == 0) {
+                    q.push(nextCourse);
+                }
+            }
+            
+        }
+        
+        if(count == numCourses) return true; 
+        return false; // Means Cycle Exists
+    }
+};
