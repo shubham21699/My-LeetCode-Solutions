c0edc6f48ae497559bf5e0700ff1beb231c4ee6b
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-27 14:17:05 +0530

    Time: 49 ms (27.46%), Space: 25.8 MB (10.66%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -6,7 +6,6 @@ public:
         if(n == 1 || k == 0) return;
         
         vector<int> num(nums);
-        
         for(int i=0 ; i<n ; i++) {
             nums[(i+k) % n] = num[i];
         }
