c41760f75565e554f4830448d14b9082df1e2293
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-29 00:00:14 +0530

    Time: 197 ms (30.28%), Space: 61.2 MB (59.19%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -5,12 +5,14 @@ public:
         int n = nums.size()-1;
         
         for(int i=0 ; i<=n ; i++) {
+            
             int index = abs(nums[i]);
             nums[index] = -1 * nums[index];
             
             // After multiplying with -1, a duplicate number gets to its original +ve value 
             // after its 2nd appearance and when that appears return absolute of that duplicate value.
             if(nums[index] > 0) return abs(nums[i]);
+            
         }
         
         return -1;
