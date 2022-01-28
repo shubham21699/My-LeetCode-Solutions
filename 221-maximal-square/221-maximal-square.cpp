68d319e48d09d3d768465c0fabb20541bdb519fe
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-01-28 15:21:05 +0530

    Time: 96 ms (19.52%), Space: 18.2 MB (11.73%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -32,18 +32,20 @@ public:
         }
         
         int result = 0;
-        
         vector<vector<int> > dp(n, vector<int>(m, 0));
         
         for(int i=0 ; i<n ; i++) {
             for(int j=0 ; j<m ; j++) {
+                
                 if(i==0 || j==0 || matrix[i][j] == '0') {
                     dp[i][j] = matrix[i][j]-'0';
                 }
                 else if(matrix[i][j] == '1') {
                     dp[i][j] = min({ dp[i-1][j], dp[i][j-1], dp[i-1][j-1] }) + 1;
                 }
+                
                 result = max(result, dp[i][j]);
+                
             }
         }
         
