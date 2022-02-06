a9707bbdec50f4838b8bea3fa903f5191d9c4f37
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-06 22:49:57 +0530

    Time: 4 ms (86.50%), Space: 10.9 MB (74.56%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,14 +4,14 @@ public:
         
         int n = nums.size();
         if(n <= 2) return n;
-        int index = 2;
+        int position = 2;
         
         for(int i=2 ; i<n ; i++) {
-            if(nums[i] != nums[index-2]) {
-                nums[index++] = nums[i];
+            if(nums[i] != nums[position-2]) {
+                nums[position++] = nums[i];
             }
         }
         
-        return index;
+        return position;
     }
 };
