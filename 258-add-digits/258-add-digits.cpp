ca47af7aecf831048087367bfd3168e1154b16ab
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-08 14:23:16 +0530

    Time: 4 ms (43.34%), Space: 5.7 MB (95.57%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num < 10) return num;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
+        
+//         OR:
+//         if(num < 10) return num;
+        
+//         while(num >= 10) {
+            
+//             int x = 0;
+            
+//             while(num) {
+//                 x += num%10;
+//                 num /= 10;
+//             }
+            
+//             num = x;
+//         }
+        
+//         return num;
+    }
+};
