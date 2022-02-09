bd46bea48e8f91d39096deff5394efa50e2569c6
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-10 00:24:18 +0530

    Time: 28 ms (46.28%), Space: 6.1 MB (7.71%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if(n == 0) return false;
+        if(n == 1) return true;
+        
+        return fmod(log10(n) / log10(3), 1) == 0;
+    }
+};
