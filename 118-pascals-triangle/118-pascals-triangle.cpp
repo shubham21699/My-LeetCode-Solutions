66a60580d22bfbce19bb8df6a1b38a745a8640e0
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-09 18:27:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (91.59%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,6 +5,7 @@ public:
         vector<vector<int> > result;
         
         for(int i=0 ; i<numRows ; i++) {
+            
             result.push_back(vector<int>(i+1, 1));
             
             for(int j=1 ; j<i ; j++) {
