6ecd70a827aa7e6574a6ddf039bfd525939beb70
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-12 21:27:36 +0530

    Time: 1681 ms (6.29%), Space: 27.4 MB (22.83%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> q;
+        q.push(beginWord);
+        int ladder = 1;
+        
+        while(!q.empty()) {
+            
+            int n = q.size();
+            
+            while(n--) {
+                
+                string word = q.front();
+                q.pop();
+                
+                if(word == endWord) return ladder;
+                
+                dict.erase(word);
+                
+                for(int i=0 ; i<word.length() ; i++) {
+                    char c = word[i];
+                    for(int j=0 ; j<26 ; j++) {
+                        word[i] = 'a' + j;
+                        if(dict.find(word) != dict.end()) {
+                            q.push(word);
+                        }
+                    }
+                    
+                    word[i] = c;
+                }
+                
+            }
+            
+            ladder++;
+        }
+        
+        // If no sequence exists
+        return 0;
+    }
+};
