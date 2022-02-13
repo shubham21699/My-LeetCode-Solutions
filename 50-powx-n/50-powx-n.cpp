9095af50e4d9bbd7f3ee79340496f4698544eb7e
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-13 15:10:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.03%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n == 0 || x == 1) return 1.0;
+        
+        if(n < 0) {
+            n = abs(n);
+            x = 1/x;
+        }
+        
+        return n%2 == 0 ? myPow(x*x, n/2) : x * myPow(x*x, n/2);
+    }
+};
