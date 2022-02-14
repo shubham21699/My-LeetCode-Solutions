ae69f5ebcdb6da3068028df3ce6116c9f9f259c5
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-14 17:17:12 +0530

    Time: 11 ms (36.31%), Space: 13 MB (86.86%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -6,7 +6,7 @@ public:
         int result = 0;
         
         for(int i=1 ; i<n ; i++) {
-            result += max(0, prices[i] - prices[i-1]);
+            result += max(prices[i] - prices[i-1], 0);
         }
         
         return result;
