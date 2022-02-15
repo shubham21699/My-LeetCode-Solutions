9f7d52b5a5c60f343107495bd6773429922d9a89
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-15 18:07:36 +0530

    Time: 89 ms (54.09%), Space: 61.5 MB (70.16%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -7,11 +7,9 @@ public:
         int mid = INT_MAX;
         
         for(int i=0 ; i<n ; i++) {
-            
             if(nums[i] <= min) min = nums[i]; // This will be the min number so far
             else if(nums[i] <= mid) mid = nums[i]; // This will be just bigger than min element get so far 
             else return true; // If current nums[i] is greater than both min and mid numbers
-            
         }
         
         return false;
