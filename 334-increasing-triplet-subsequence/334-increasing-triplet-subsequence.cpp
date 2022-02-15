6825cc6d22ddbcdae35d6dc1f8bd5d14930c9315
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-15 18:07:10 +0530

    Time: 134 ms (17.69%), Space: 61.6 MB (70.16%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        
+        int n = nums.size();
+        int min = INT_MAX;
+        int mid = INT_MAX;
+        
+        for(int i=0 ; i<n ; i++) {
+            
+            if(nums[i] <= min) min = nums[i]; // This will be the min number so far
+            else if(nums[i] <= mid) mid = nums[i]; // This will be just bigger than min element get so far 
+            else return true; // If current nums[i] is greater than both min and mid numbers
+            
+        }
+        
+        return false;
+    }
+};
