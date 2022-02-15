83b912857f22be2c12139bc19837bd09c734b364
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-15 12:39:34 +0530

    Time: 16 ms (33.48%), Space: 7.7 MB (89.11%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calculate(string s) {
+        
+        int n = s.length();
+        if(n == 1) return s[0]-'0';
+        if(n == 2) return stoi(s);
+        
+        int result = 0;
+        int temp = 0;
+        int i = 0;
+        char op = '+';
+        
+        while(i < n) {
+            
+            if(isdigit(s[i])) {
+                int num = 0;
+                
+                while(i<n && isdigit(s[i])) {
+                    num = num*10 + (s[i]-'0');
+                    i++;
+                }
+                
+                if(op == '+' || op == '-') {
+                    result += temp;
+                    temp = num * (op == '-' ? -1 : 1);
+                }
+                else if(op == '*') {
+                    temp *= num;
+                }
+                else if(op == '/') {
+                    temp /= num;
+                }
+                
+                continue;
+            }
+            else if(s[i] != ' ') op = s[i];
+            
+            i++;
+        }
+        
+        result += temp;
+        return result;
+    }
+};
