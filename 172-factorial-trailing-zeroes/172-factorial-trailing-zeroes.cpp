b4c7ad3a3928273ac6cc802b77c547285bc403fd
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-15 16:27:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.08%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -7,7 +7,7 @@ public:
         int count = 0;
         // Check for multiples of 5 lies till given number 'n'
         // Like mutiples of 5, 5x5, 5x5x5, ... in 'n!'
-        // because no. of mutiples of 5 = no. of trailing zeroes will be there in n!.
+        // because no. of mutiples of 5 = no. of trailing zeroes will be there in 'n!'.
         for(int i=5 ; i<=n ; i*=5) {
             count += (n/i);
         }
