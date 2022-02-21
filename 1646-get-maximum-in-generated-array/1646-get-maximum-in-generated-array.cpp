d46e25c8ccfe918faeede0a459da737d5fba4ec9
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-21 13:04:09 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (24.28%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -4,14 +4,15 @@ public:
         
         if(n <= 1) return n;
         
-        vector<int> v(n+1);
-        v[0] = 0;
+        vector<int> v(n+1, 0);
         v[1] = 1;
+        int maxi = 1;
         
         for(int i=2 ; i<=n ; i++) {
             v[i] = i%2==0 ? v[i/2] : v[i/2] + v[i/2 + 1];
+            maxi = max(maxi, v[i]);
         }
         
-        return *max_element(v.begin(), v.end());
+        return maxi;
     }
 };
