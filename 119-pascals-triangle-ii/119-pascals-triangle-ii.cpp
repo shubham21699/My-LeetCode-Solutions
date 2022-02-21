f9a14e00169c64fe3e397a04581640ade82f10e4
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-02-21 10:26:05 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.74%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -9,6 +9,7 @@ public:
         vector<vector<int>> pascal;
         
         for(int i=0 ; i<=rowIndex ; i++) {
+            
             pascal.push_back(vector<int>(i+1, 1));
             
             for(int j=1 ; j<i ; j++) {
