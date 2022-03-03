626748cdd163b50fdbca33c81e218e074b537aca
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-03-03 10:39:25 +0530

    Time: 5 ms (27.51%), Space: 7.3 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -10,15 +10,13 @@ public:
         }
         
         vector<int> dp(n, 0);
+        int count = 0;
         
         for(int i=2 ; i<n ; i++) {
             if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) {
                 dp[i] = dp[i-1] + 1;
             }
-        }
-        
-        int count = 0;
-        for(int i=0 ; i<n ; i++) {
+            
             count += dp[i];
         }
         
