22caac889081c07fec9234e95b91321b23938504
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-03-06 12:02:14 +0530

    Time: 4 ms (44.03%), Space: 6 MB (53.14%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        
+        if(n == 1) return 1;
+        
+        long long int result = 1;
+        
+        for(int i=1 ; i<=n ; i++) {
+            // Here, i for factorial and (2*i-1) for series
+            result *= (i * (2*i - 1));
+            result %= 1000000007;
+        }
+        
+        return result;
+    }
+};
