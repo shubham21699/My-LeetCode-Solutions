4a6448a4680c38fb25769438d1a87a562f4c3236
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-03-15 11:47:34 +0530

    Time: 41 ms (31.54%), Space: 10.4 MB (85.38%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -10,7 +10,7 @@ public:
             if(s[i] == '(') st.push(i);
             else if(s[i] == ')') {
                 if(!st.empty()) st.pop();
-                else s[i] = '#'; // Invalid parantheses position
+                else s[i] = '#'; // Invalid parantheses position.
             }
         }
         
@@ -20,6 +20,7 @@ public:
             st.pop();
         }
         
+        // Removing all the '#' from invalids parantheses position.
         s.erase(remove(s.begin(), s.end(), '#'), s.end());
         return s;
     }
