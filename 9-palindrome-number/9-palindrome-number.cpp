e505fb087afe8a9af8448fdc989c14b90a1c1450
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-04-05 14:04:23 +0530

    Time: 30 ms (21.98%), Space: 5.9 MB (33.75%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if(x < 0) return false;
+        long long int rev = 0;
+        int copy = x;
+        
+        while(copy) {
+            rev = rev*10 + (copy%10);
+            copy /= 10;
+        }
+        
+        if(rev == x) return true;
+        return false;
+    }
+};
