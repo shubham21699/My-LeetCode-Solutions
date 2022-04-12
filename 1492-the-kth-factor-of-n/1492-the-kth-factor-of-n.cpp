0736dbd14c56a634cefe20c075e932910479a62c
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-04-12 12:41:47 +0530

    Time: 3 ms (35.38%), Space: 6 MB (60.19%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        
+        int kth_factor = -1;
+        
+        for(int i=1 ; i<=n && k>0 ; i++) {
+            if(n % i == 0) {
+                kth_factor = i;
+                k--;
+            }
+        }
+        
+        if(k) return -1;
+        return kth_factor;
+    }
+};
