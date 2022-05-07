55826947af9f5025c3353d0516de0499cb05de33
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-05-07 12:33:01 +0530

    Time: 873 ms (16.41%), Space: 10 MB (46.65%) - LeetHub

---
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
+        
+        int n = students.size();
+        vector<int> permute(n);
+        
+        for(int i=0 ; i<n ; i++) permute[i] = i;
+        
+        int maxScore = 0;
+        
+        do {
+            
+            int score = 0;
+            
+            for(int i=0 ; i<n ; i++) {
+                
+                int bit = 0;
+                int k = permute[i];
+                
+                for(int j=0 ; j<students[i].size() ; j++) {
+                    bit += !(students[i][j] xor mentors[k][j]);
+                }
+                
+                score += bit;
+            }
+            
+            maxScore = max(maxScore, score);
+            
+        } while(next_permutation(permute.begin(), permute.end()));
+        
+        return maxScore;
+    }
+};
