452cd06479a0fa133a36b0ca8a6814f2a0c5091d
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-05-16 12:54:02 +0530

    Time: 73 ms (75.42%), Space: 19.3 MB (70.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -10,27 +10,59 @@ public:
         
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
         
-        int row = n;
-        int col = n;
-        queue<pair<int, int>> queue;
-        queue.push(make_pair(0,0));
-        vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+        queue<pair<int, int>> q;
+        q.push({ 0, 0 });
         grid[0][0] = 1;
         
-        while(!queue.empty()){
-            auto curr = queue.front();
-            int x = curr.first, y = curr.second;
-            if( x == row-1 && y == col-1) return grid[x][y];
+        while(!q.empty()) {
+            
+            int size = q.size();
+            
+            while(size--) {
+            
+                auto p = q.front();
+                q.pop();
 
-            for(auto direction : directions){
-                int nx = x + direction[0];
-                int ny = y + direction[1];
-                if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                    queue.push(make_pair(nx,ny));
-                    grid[nx][ny] = grid[x][y] + 1;
+                int i = p.first;
+                int j = p.second;
+
+                if(i == n-1 && j == n-1) return grid[i][j];
+
+                if(i+1<n && j<n && i>=0 && j>=0 && grid[i+1][j] == 0) {
+                    q.push({ i+1, j });
+                    grid[i+1][j] = grid[i][j] + 1;
                 }
+                if(i<n && j+1<n && i>=0 && j>=0 && grid[i][j+1] == 0) {
+                    q.push({ i, j+1 });
+                    grid[i][j+1] = grid[i][j] + 1;
+                }
+                if(i<n && j<n && i-1>=0 && j>=0 && grid[i-1][j] == 0) {
+                    q.push({ i-1, j });
+                    grid[i-1][j] = grid[i][j] + 1;
+                }
+                if(i<n && j<n && i>=0 && j-1>=0 && grid[i][j-1] == 0) {
+                    q.push({ i, j-1 });
+                    grid[i][j-1] = grid[i][j] + 1;
+                }
+                if(i+1<n && j+1<n && i>=0 && j>=0 && grid[i+1][j+1] == 0) {
+                    q.push({ i+1, j+1 });
+                    grid[i+1][j+1] = grid[i][j] + 1;
+                }
+                if(i+1<n && j<n && i>=0 && j-1>=0 && grid[i+1][j-1] == 0) {
+                    q.push({ i+1, j-1 });
+                    grid[i+1][j-1] = grid[i][j] + 1;
+                }
+                if(i<n && j<n && i-1>=0 && j-1>=0 && grid[i-1][j-1] == 0) {
+                    q.push({ i-1, j-1 });
+                    grid[i-1][j-1] = grid[i][j] + 1;
+                }
+                if(i<n && j+1<n && i-1>=0 && j>=0 && grid[i-1][j+1] == 0) {
+                    q.push({ i-1, j+1 });
+                    grid[i-1][j+1] = grid[i][j] + 1;
+                }
+
             }
-            queue.pop();
+            
         }
         
         return -1;
