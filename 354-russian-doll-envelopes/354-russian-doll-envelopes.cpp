c2a850b6bb001e869b4f9f6919d23a0d774e8cd5
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-07-30 18:56:54 +0530

    Time: 450 ms (82.73%), Space: 77.5 MB (66.69%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,38 +1,41 @@
 class Solution {
-    int check(vector<vector<int> >& envelopes, int index, int n, vector<int> prev) {
-        
-        if(index >= n) return 0;
-        if(envelopes[index][0] > prev[0] && envelopes[index][1] > prev[1]) return 1 + check(envelopes, index+1, n, envelopes[index]);
-         
-        return max(check(envelopes, index+1, n, prev), check(envelopes, index+1, n, envelopes[index]));
-    }
-    
 public:
-    static bool compare(vector<int>&a , vector<int>& b) {
-      return a[0]==b[0]?a[1]>b[1]:a[0]<b[0];
+    int russian(vector<vector<int>>& envelopes, int i, int n, int prev, vector<vector<int>>& dp) {
+        
+        if(i >= n) return 0;
+        if(dp[i][prev+1] != -1) return dp[i][prev+1]; 
+        
+        int include = 0;
+        if(prev == -1 || envelopes[i][0] > envelopes[prev][0] && envelopes[i][1] > envelopes[prev][1]) 
+            include = 1 + russian(envelopes, i+1, n, i, dp);
+        
+        int exclude = 0 + russian(envelopes, i+1, n, prev, dp);
+        return dp[i][prev+1] = max(include, exclude);
     }
     
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         
-        sort(envelopes.begin(),envelopes.end(),compare);
-         vector<int> dp;
-         
-        for(auto i:envelopes) {
-            auto it=lower_bound(dp.begin(),dp.end(),i[1]);
-            if(it==dp.end())
-              dp.push_back(i[1]);
-            else
-              *it=i[1];
-          }
-          return dp.size();
+        int n = envelopes.size();
+        if(n == 1) return 1;
         
-//         int n = envelopes.size();
+        sort(envelopes.begin(), envelopes.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+        });
         
-//         sort(envelopes.begin(), envelopes.end(), [](vector<int> a, vector<int> b) {
-//             return a[0] == b[0] ? a[1] < b[1] : a[0] < b[0];
-//         });
+        vector<int> dp;
         
-//         return check(envelopes, 1, n, envelopes[0]) + 1;
+        for(int i=0 ; i<n ; i++) {
+            
+            auto it = lower_bound(dp.begin(), dp.end(), envelopes[i][1]);
+            if(it == dp.end()) dp.push_back(envelopes[i][1]);
+            else *it = envelopes[i][1];
+            
+        }
         
+        return dp.size();
+        
+        // // OR: Will give TLE
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // return russian(envelopes, 0, n, -1, dp);
     }
 };
