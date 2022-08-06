8ed5d965d054e605d75379459ccfc266a91a4343
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-08-06 22:27:19 +0530

    Time: 40 ms (19.28%), Space: 9 MB (38.99%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
+    int solve(int l, int r, vector<vector<int>>& dp, vector<int>& arr) {
+        
+        if(l >= r) return 0;
+        if(dp[l][r] != -1) return dp[l][r];
+        
+        int ans = INT_MAX;
+        
+        for(int i=l ; i<r ; i++) {
+            
+            int leftMax = *max_element(arr.begin()+l, arr.begin()+i+1);
+            int rightMax = *max_element(arr.begin()+i+1, arr.begin()+r+1);
+            
+            ans = min(ans, leftMax*rightMax + solve(l, i, dp, arr) + solve(i+1, r, dp, arr));
+        }
+        
+        return dp[l][r] = ans;
+    }
+    
     int mctFromLeafValues(vector<int>& arr) {
         
         int n = arr.size();
         if(n == 2) return arr[0]*arr[1];
         
-        // Using Monotonic Stack Approach:
-        stack<int> s;
-        int result = 0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(0, n-1, dp, arr);
         
-        for(int i=0 ; i<n ; i++) {
-            
-            while(!s.empty() && s.top() <= arr[i]) {
-                int x = s.top();
-                s.pop();
-                result += x * min(s.empty() ? INT_MAX : s.top(), arr[i]);
-            }
-            
-            s.push(arr[i]);
-            
-        }
+//         // OR - Using Monotonic Stack Approach:
+//         stack<int> s;
+//         int result = 0;
         
-        int top = s.top();
-        s.pop();
+//         for(int i=0 ; i<n ; i++) {
+            
+//             while(!s.empty() && s.top() <= arr[i]) {
+//                 int x = s.top();
+//                 s.pop();
+//                 result += x * min(s.empty() ? INT_MAX : s.top(), arr[i]);
+//             }
+            
+//             s.push(arr[i]);
+            
+//         }
         
-        while(!s.empty()) {
-            int x = s.top();
-            s.pop();
-            result += x * min(s.empty() ? INT_MAX : s.top(), top);
-            top = x;
-        }
+//         int top = s.top();
+//         s.pop();
+        
+//         while(!s.empty()) {
+//             int x = s.top();
+//             s.pop();
+//             result += x * min(s.empty() ? INT_MAX : s.top(), top);
+//             top = x;
+//         }
      
-        return result;
+//         return result;
     }
 };
