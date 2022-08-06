cda2d72c96c134a67515b8dfa1f76cb8d117ecb7
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-08-06 18:50:43 +0530

    Time: 5 ms (99.52%), Space: 8.2 MB (37.93%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int solve(int l, int r, vector<vector<int>>& dp) {
+        
+        if(l >= r) return 0;
+        if(dp[l][r] != 0) return dp[l][r];
+        
+        // Binary Search Approach -
+        int ans = INT_MAX;
+        
+        for(int i=r ; i>=(l+r)/2 ; i--) {
+            
+            int left = solve(l, i - 1, dp);
+            int right = solve(i + 1, r, dp);
+            
+            int ansForCurrVal = i + max(left, right);
+            
+            if(ansForCurrVal > ans) break;
+            ans = ansForCurrVal;
+        }
+        
+        return dp[l][r] = ans;
+    }
+    
     int getMoneyAmount(int n) {
         
         if(n == 1) return 0;
@@ -7,22 +29,25 @@ public:
         
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         
-        for(int len=2 ; len<=n ; len++) {
-            for(int begin=0 ; begin<=n-len ; begin++) {
-                int end = begin + len;
-                
-                for(int i=begin ; i<end ; i++) {
-                    
-                    int numPicked = i+1;
-                    
-                    if(i == begin) dp[begin][end] = numPicked + dp[begin + 1][end];
-                    else dp[begin][end] = min(dp[begin][end], max(dp[begin][i], dp[i+1][end]) + numPicked);
-                    
-                }
-                
-            }
-        }
+        return solve(1, n, dp);
         
-        return dp[0][n];
+//         // OR:
+//         for(int len=2 ; len<=n ; len++) {
+//             for(int begin=0 ; begin<=n-len ; begin++) {
+//                 int end = begin + len;
+                
+//                 for(int i=begin ; i<end ; i++) {
+                    
+//                     int numPicked = i+1;
+                    
+//                     if(i == begin) dp[begin][end] = numPicked + dp[begin + 1][end];
+//                     else dp[begin][end] = min(dp[begin][end], max(dp[begin][i], dp[i+1][end]) + numPicked);
+                    
+//                 }
+                
+//             }
+//         }
+        
+//         return dp[0][n];
     }
 };
