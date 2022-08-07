538437d778e355bdb4b25878b82fac2e25f9151c
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-08-07 17:56:22 +0530

    Time: 61 ms (60.00%), Space: 16.6 MB (60.00%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        
+        int n = items1.size();
+        int m = items2.size();
+        unordered_map<int, int> mp;
+        
+        for(int i=0 ; i<n ; i++) mp[items1[i][0]] = items1[i][1];
+        for(int i=0 ; i<m ; i++) mp[items2[i][0]] += items2[i][1];
+        
+        vector<vector<int>> res;
+        for(auto it=mp.begin() ; it!=mp.end() ; it++) {
+            res.push_back({ it->first, it->second });
+        }
+        
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
