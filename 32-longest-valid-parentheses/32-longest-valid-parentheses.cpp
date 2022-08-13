bbe6c839c23b6866ca878decc70206aa8b30d002
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-08-13 18:29:33 +0530

    Time: 4 ms (78.35%), Space: 6.9 MB (88.50%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,26 +3,60 @@ public:
     int longestValidParentheses(string s) {
         
         int n = s.length();
-        if(n == 0) return 0;
+        if(n <= 1) return 0;
+        if(n == 2) return s == "()" ? 2 : 0;
         
-        stack<int> st;
-        st.push(-1);
-        int result = 0;
+        int open = 0, close = 0;
+        int maxLen = 0;
         
         for(int i=0 ; i<n ; i++) {
             
-            if(s[i] == ')') {
-                
-                st.pop();
-                
-                if(!st.empty()) result = max(result, i - st.top());
-                else st.push(i);
-                
+            if(s[i] == '(') open++;
+            else close++;
+            
+            if(open == close) maxLen = max(maxLen, open+close);
+            else if(close > open) {
+                open = 0;
+                close = 0;
             }
-            else st.push(i);
             
         }
         
-        return result;
+        open = 0;
+        close = 0;
+        
+        for(int i=n-1 ; i>=0 ; i--) {
+            
+            if(s[i] == '(') open++;
+            else close++;
+            
+            if(open == close) maxLen = max(maxLen, open+close);
+            else if(open > close) {
+                open = 0;
+                close = 0;
+            }
+            
+        }
+        
+        return maxLen;
+        
+//         // OR:
+//         stack<int> st;
+//         st.push(-1);
+//         int maxLen = 0;
+        
+//         for(int i=0 ; i<n ; i++) {
+            
+//             if(s[i] == '(') st.push(i);
+//             else {  
+//                 st.pop();
+                
+//                 if(!st.empty()) maxLen = max(maxLen, i-st.top());
+//                 else st.push(i);
+//             }
+            
+//         }
+        
+//         return maxLen;
     }
 };
