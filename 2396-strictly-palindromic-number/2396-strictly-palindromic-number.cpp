681592300b157a7eaf8c21454b0ef0b6d3f8375b
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-09-04 15:10:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        
+        // It's not possible to have strictly palindromic integer 'n' for every base between 2 and n-2
+        return false;
+    }
+};
