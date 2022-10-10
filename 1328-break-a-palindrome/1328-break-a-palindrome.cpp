00b57deed4d863e54485196dc83b82b9b79d34bb
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2022-10-10 10:02:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        
+        int n = palindrome.length();
+        if(n == 1) return "";
+        
+        bool flag = false;
+        
+        for(int i=0 ; i<n/2 ; i++) {
+            
+            if(palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                flag = true;
+                break;
+            }
+            
+        }
+        
+        if(!flag) palindrome[n-1] = 'b';
+        return palindrome;
+    }
+};
