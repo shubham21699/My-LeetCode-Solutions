ad85ea56316290be01e617ce45449955c5fa93fd
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2023-08-13 12:16:23 +0530

    Time: 130 ms (88.00%), Space: 92.1 MB (48.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int i, int j, int n, unordered_map<int, unordered_map<int, int>>& dp) {
+    bool partition(vector<int>& nums, int i, int n, vector<int>& dp) {
         
         if(i >= n) return true;
-        if(j >= n || j-i+1 >= 4) return false;
-        if(dp.find(i) != dp.end() && dp[i].find(j) != dp[j].end()) return dp[i][j];
+        if(dp[i] != -1) return dp[i];
         
-        bool ans = false;
+        bool isGood = false;
         
-        if(j-i+1 == 2) {
-            if(nums[i] == nums[j]) ans |= check(nums, j+1, j+1, n, dp);
+        if(i < n-1 && nums[i] == nums[i+1]) {
+            isGood |= partition(nums, i+2, n, dp);
+        }
+        if(i < n-2 && nums[i] == nums[i+1] && nums[i] == nums[i+2]) {
+            isGood |= partition(nums, i+3, n, dp);
+        }
+        if(i < n-2 && nums[i+1]-nums[i] == 1 && nums[i+2]-nums[i+1] == 1) {
+            isGood |= partition(nums, i+3, n, dp);
         }
         
-        if(j-i+1 == 3) {
-            if(nums[i] == nums[i+1] && nums[i] == nums[j]) ans |= check(nums, j+1, j+1, n, dp);
-            if(nums[i+1]-nums[i] == 1 && nums[j]-nums[i+1] == 1) ans |= check(nums, j+1, j+1, n, dp);
-        }
-        
-        ans |= check(nums, i, j+1, n, dp);
-        return dp[i][j] = ans;
+        return dp[i] = isGood;
     }
     
     bool validPartition(vector<int>& nums) {
         
         int n = nums.size();
-        if(n == 2) return nums[0]==nums[1];
         
-       unordered_map<int, unordered_map<int, int>> dp;
-        return check(nums, 0, 0, n, dp);
+        vector<int> dp(n, -1);
+        return partition(nums, 0, n, dp);
     }
 };
