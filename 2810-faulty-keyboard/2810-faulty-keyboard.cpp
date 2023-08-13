20b805c757283eb22437c8742dcf8d44c958a3c9
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2023-08-13 12:19:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (77.79%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string finalString(string s) {
+        
+        int n = s.length();
+        string res = "";
+        
+        for(int i=0 ; i<n ; i++) {
+            if(s[i] == 'i') reverse(res.begin(), res.end());
+            else res += s[i];
+        }
+        
+        return res;
+    }
+};
