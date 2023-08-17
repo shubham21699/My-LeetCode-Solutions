1e49b4d3c4bb015f1796acb04351748264472134
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2023-08-17 17:50:36 +0530

    Time: 0 ms (100.00%), Space: 17.6 MB (77.36%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canSplitArray(vector<int>& nums, int m) {
+        
+        // If n == 1, done.
+        // If n == 2, split once, done.
+        // Otherwise we must have a middle status, that one array has length 2 and nums[i] + nums[i+1] >= m.
+        
+        int n = nums.size();
+        if(n == 1 || n == 2) return true;
+        
+        for(int i=0 ; i<n-1 ; i++) {
+            if(nums[i] + nums[i+1] >= m) return true;
+        }
+        
+        return false;
+    }
+};
