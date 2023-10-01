f122ba4529f268ea6ac4b010da64a7f231a6ead4
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2023-10-01 15:06:04 +0530

    Time: 3 ms (92.31%), Space: 18.2 MB (7.69%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2873-maximum-value-of-an-ordered-triplet-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2873-maximum-value-of-an-ordered-triplet-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        
+        int n = nums.size();
+        long long result = 0;
+        
+        // For each nums[i] in A, we update several values
+        // result is the maximum value of a triplet (A[i] - A[j]) * A[k].
+        // maxab is the maximum value of pair A[i] - A[j]
+        // maxa is the maximum value of element A[i].
+        
+        long long maxab = 0, maxa = 0;
+        
+        for(int i=0 ; i<n ; i++) {
+            result = max(result, 1LL*maxab*nums[i]);
+            maxab = max(maxab, maxa - nums[i]);
+            maxa = max(maxa, 1LL*nums[i]);
+        }
+        
+        // // OR: O(n^3)
+        // for(int i=0 ; i<n-2 ; i++) {
+        //     for(int j=i+1 ; j<n-1 ; j++) {
+        //         for(int k=j+1 ; k<n ; k++) {
+        //             long long val = (long long) (nums[i] - nums[j]) * nums[k];
+        //             result = max(result, val);
+        //         }
+        //     }
+        // }
+        
+        return result;
+    }
+};
