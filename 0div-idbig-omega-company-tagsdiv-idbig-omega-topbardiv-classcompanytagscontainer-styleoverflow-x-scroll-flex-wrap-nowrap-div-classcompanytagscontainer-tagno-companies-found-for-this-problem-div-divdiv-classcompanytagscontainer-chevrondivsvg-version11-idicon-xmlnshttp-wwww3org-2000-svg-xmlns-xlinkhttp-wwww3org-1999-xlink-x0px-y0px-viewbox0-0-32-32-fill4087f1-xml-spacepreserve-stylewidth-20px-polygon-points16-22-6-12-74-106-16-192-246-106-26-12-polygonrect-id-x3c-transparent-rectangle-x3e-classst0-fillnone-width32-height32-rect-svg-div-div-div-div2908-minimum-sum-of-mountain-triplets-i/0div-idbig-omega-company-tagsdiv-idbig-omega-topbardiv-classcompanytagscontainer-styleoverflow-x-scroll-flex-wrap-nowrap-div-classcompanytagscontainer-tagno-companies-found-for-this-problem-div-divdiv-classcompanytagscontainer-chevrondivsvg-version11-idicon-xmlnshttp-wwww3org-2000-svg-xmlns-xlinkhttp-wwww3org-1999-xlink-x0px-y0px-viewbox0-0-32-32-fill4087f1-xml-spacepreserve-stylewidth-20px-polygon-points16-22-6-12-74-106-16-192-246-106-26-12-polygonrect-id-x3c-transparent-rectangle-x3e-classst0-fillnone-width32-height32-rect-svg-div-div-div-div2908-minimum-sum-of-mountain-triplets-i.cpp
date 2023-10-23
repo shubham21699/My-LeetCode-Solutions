db947c38490dc55d1de276c15fb18a6d7ea0ec44
Shubham Goel <71596140+shubham21699@users.noreply.github.com>
2023-10-23 14:04:53 +0530

    Time: 0 ms (100.00%), Space: 21.2 MB (10.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2908-minimum-sum-of-mountain-triplets-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2908-minimum-sum-of-mountain-triplets-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+        
+        int n = nums.size();
+        int result = INT_MAX;
+        vector<int> rightMin = nums;
+        
+        for(int i=n-2 ; i>=0 ; i--) {
+            rightMin[i] = min(rightMin[i], rightMin[i+1]);
+        }
+        
+        int leftMin = nums[0];
+        
+        for(int i=1 ; i<n-1 ; i++) {
+            if(nums[i] > leftMin && nums[i] > rightMin[i]) {
+                result = min(result, leftMin + nums[i] + rightMin[i]);
+            }
+            
+            leftMin = min(leftMin, nums[i]);
+        }
+        
+        return result == INT_MAX ? -1 : result;
+        
+//         // OR: Brute Force
+//         for(int i=0 ; i<n-2 ; i++) {
+//             for(int j=i+1 ; j<n-1 ; j++) {
+//                 for(int k=j+1 ; k<n ; k++) {
+//                     if(nums[i] < nums[j] && nums[k] < nums[j]) {
+//                         result = min(result, nums[i] + nums[j] + nums[k]);
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return result == INT_MAX ? -1 : result;
+    }
+};
